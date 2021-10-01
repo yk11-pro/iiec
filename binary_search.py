5cfb67737a4780c3cfc12eadc529745c020e7de5
smarty-boy <91757283+smarty-boy@users.noreply.github.com>
2021-10-02 00:59:55 +0530

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -26,15 +26,15 @@ def binary_search(list1, n):
     return -1  
   
   
-# Initial list1  
-list1 = [12, 24, 32, 39, 45, 50, 54]  
-n = 45  
+# # Initial list1  
+# list1 = [12, 24, 32, 39, 45, 50, 54]  
+# n = 45  
   
-# Function call   
-result = binary_search(list1, n)  
+# # Function call   
+# result = binary_search(list1, n)  
  
-#final step for verification of code
-if result != -1:  
-    print("Element is present at index", str(result))  
-else:  
-    print("Element is not present in list1") 
+# #final step for verification of code
+# if result != -1:  
+#     print("Element is present at index", str(result))  
+# else:  
+#     print("Element is not present in list1") 
